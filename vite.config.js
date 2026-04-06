a64fab484461f28ddc3cbb046d4d6aab2b39cb63
angovnable <brywanyoike@gmail.com>
2026-04-06 13:53:06 +0300

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/timetable-app/',  // Must match your GitHub repo name
+  base: '/',  // Must match your GitHub repo name
   plugins: [
     react(),
     VitePWA({
